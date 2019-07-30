4d9f32c540849d6d59a9ebafc31252e3e86261c3
KentFan <cjfan2005@gmail.com>
2019-07-30 14:37:31 +0800

    separate controller and config

--- src/main/java/spring/boot/practice/Application.java
+++ src/main/java/spring/boot/practice/Application.java
@@ -29,7 +29,7 @@ public class Application {
 			repository.save(new Customer("Jack", "Bauer"));
 			repository.save(new Customer("Chloe", "O'Brian"));
 			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("David", "Palmer2"));
 			repository.save(new Customer("Michelle", "Dessler"));
 
 			// fetch all customers
--- src/main/java/spring/boot/practice/controller/MainController.java
+++ src/main/java/spring/boot/practice/controller/MainController.java
@@ -1,4 +1,4 @@
-package spring.boot.practice;
+package spring.boot.practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
