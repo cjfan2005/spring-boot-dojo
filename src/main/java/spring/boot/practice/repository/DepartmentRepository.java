59efa43203f1efced826ed3cba1e967613d2c3e4
KentFan <cjfan2005@gmail.com>
2019-08-08 11:41:31 +0800

    given Query annotation

--- src/main/java/spring/boot/practice/repository/DepartmentRepository.java
+++ src/main/java/spring/boot/practice/repository/DepartmentRepository.java
@@ -2,6 +2,7 @@ package spring.boot.practice.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import spring.boot.practice.dto.DeptUserDto;
@@ -9,8 +10,9 @@ import spring.boot.practice.model.Department;
 
 public interface DepartmentRepository extends CrudRepository<Department, Integer> {
 
+	@Query("SELECT d.name, u.name, u.email FROM Department d LEFT JOIN d.user u")
 	List<DeptUserDto> fetchUserDeptDataLeftJoin();
 	
-	
+	@Query("SELECT d.name, u.name, u.email FROM Department d RIGHT JOIN d.user u")
     List<DeptUserDto> fetchUserDeptDataRightJoin();
 }
